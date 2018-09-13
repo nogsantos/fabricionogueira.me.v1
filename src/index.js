0edf8c112a3a644f822205cce75f3d72f55fd59f
Fabricio Nogueira <fabricio.santos@treasy.com.br>
2018-09-13 09:29:21 -0300

    fix: Layout
    Corrigindo a altura do menu footer. Desabilitando o debug do redux em produção

--- src/index.js
+++ src/index.js
@@ -1,63 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ApolloProvider } from 'react-apollo'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ApolloProvider } from "react-apollo";
 
-import { createStore } from 'redux';
-import { ApolloClient } from 'apollo-client'
-import { ApolloLink } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { onError } from 'apollo-link-error';
+import { createStore } from "redux";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { onError } from "apollo-link-error";
 
-import reducer from './reducers';
-import './style.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import reducer from "./reducers";
+import "./style.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 /**
  * Apollo conf
  */
 const httpLink = new HttpLink({
     uri: process.env.REACT_APP_API_ENDPOINT,
     headers: {
-        authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+        authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`
     }
 });
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors){
+    if (graphQLErrors) {
         graphQLErrors.map(({ message, locations, path }) =>
             console.log(
-                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-            ),
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            )
         );
     }
     if (networkError) {
         console.log(`[Network error]: ${networkError}`);
     }
 });
-const link = ApolloLink.from([
-    errorLink,
-    httpLink,
-]);
+const link = ApolloLink.from([errorLink, httpLink]);
 
 const cache = new InMemoryCache({
     logger: console.log,
-    loggerEnabled: true,
+    loggerEnabled: true
 });
 
 const client = new ApolloClient({
     link,
-    cache,
+    cache
 });
-
+const devMode =
+    process.env.REACT_APP_ENV && process.env.REACT_APP_ENV === "development";
+/* eslint-disable */
+const devTools = [];
+if (devMode) {
+    // Redux dev tools
+    devTools.push(
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+}
+/* eslint-enable */
 /**
  * Reducers
  */
-const store = createStore(
-    reducer, /* preloadedState, */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(reducer /* preloadedState, */, ...devTools);
 /**
  * Render
  */
@@ -69,6 +74,6 @@ ReactDOM.render(
             </ApolloProvider>
         </BrowserRouter>
     </Provider>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
 registerServiceWorker();
